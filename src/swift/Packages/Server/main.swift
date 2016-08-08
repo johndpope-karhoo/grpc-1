350d19ee83a6d939c884efffe8d0b9577a7b655d
Tim Burks <timburks@google.com>
2016-08-08 01:34:51 +0000

    Updated Linux Swift server sample

--- src/swift/Packages/Server/main.swift
+++ src/swift/Packages/Server/main.swift
@@ -7,20 +7,22 @@ do {
   server.start()
   var running = true
   while(running) {
-    let requestHandler = server.getNextRequest()
-    print("HOST:", requestHandler.host())
-    print("METHOD:", requestHandler.method())
-    let initialMetadata = requestHandler.requestMetadata
-    for i in 0..<initialMetadata.count() {
-      print("INITIAL METADATA ->", initialMetadata.key(index:i), ":", initialMetadata.value(index:i))
+    let (status, requestHandler) = server.getNextRequest(timeout:600)
+    if let requestHandler = requestHandler {
+      print("HOST:", requestHandler.host())
+      print("METHOD:", requestHandler.method())
+      let initialMetadata = requestHandler.requestMetadata
+      for i in 0..<initialMetadata.count() {
+        print("INITIAL METADATA ->", initialMetadata.key(index:i), ":", initialMetadata.value(index:i))
+      }
+  
+      let (_, message) = requestHandler.receiveMessage()
+      print("MESSAGE", message!.string())
+      if requestHandler.method() == "/quit" {
+        running = false
+      }
+      let _ = requestHandler.sendResponse(message:ByteBuffer(string:"thank you very much!"))
     }
-
-    let (_, message) = requestHandler.receiveMessage()
-    print("MESSAGE", message!.string())
-    if requestHandler.method() == "/quit" {
-      running = false
-    }
-    let _ = requestHandler.sendResponse(message:ByteBuffer(string:"thank you very much!"))
   }
 }
 gRPC.shutdown()
